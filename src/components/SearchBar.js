12a88f334b7e31a549386b8ef9b59b849f4f812b
dayeoni <j84512@naver.com>
2025-04-30 23:58:26 +0900

    feat: 검색 창 기능 구현

---
+++ src/components/SearchBar.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./SearchBar.css"
+import Icon from '../assets/images/common/SearchIcon.png';
+
+function SearchBar(){
+    const [query, setQuery] = useState("");
+    const keys = ["title", "content"];
+    const onChange = (e) => {
+        setQuery(e.target.value.toLowerCase());
+    }
+    const search = (data) => {
+        return data.filter((item) =>
+            keys.some((key) => item[key].toLowerCase().includes(query))
+        );
+    };
+
+    const navigate = useNavigate();
+    const submit = (e) => {
+        e.preventDefault();
+        // 검색 결과 시 이동할 url (임시로 지정)
+        navigate(`/search?query=${encodeURIComponent(query)}`);
+        console.log(query);
+    }
+    
+    return(
+        <form className="search-container" onSubmit={submit} >
+            <img className="search-icon" src={Icon} alt="search icon" />
+            <input
+                className="search-input"
+                type="text" 
+                value={query}
+                onChange={onChange}
+                
+            />
+        </form>
+    )
+}
+
+export default SearchBar; 
