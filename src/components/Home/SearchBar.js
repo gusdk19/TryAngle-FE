ba36e30cf3569094ade2284a8eecdfa10a685273
dayeoni <j84512@naver.com>
2025-05-08 22:23:45 +0900

    feat: 검색바 수정

--- src/components/Home/SearchBar.js
+++ src/components/Home/SearchBar.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import "../../../styles/Home/SearchBar.css";
-import Icon from "../../../assets/images/common/SearchIcon.png";
+import "../../styles/Home/SearchBar.css";
+import Icon from "../../assets/images/common/SearchIcon.png";
 
 function SearchBar(){
     const [query, setQuery] = useState("");
