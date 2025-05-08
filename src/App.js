5e9ffb63f0fa87eccddec27f0a353205fab8ac99
dayeoni <j84512@naver.com>
2025-05-08 22:25:51 +0900

    chore: 브랜치 분리위해 수정 사항 취소

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {/*<Route path="/alarm" element={<Alarm />} /> */}
-        { /*<Route path="/challenge/add" element={<ChallengeAdd />} />*/}
+        {/*<Route path="/add-challenge" element={<ChallengeAdd />} />*/}
       </Routes>
     </BrowserRouter>
   );
