0582355d433a90b6bed6bf085e8cc802205c2490
dayeoni <j84512@naver.com>
2025-05-20 23:48:06 +0900

    feat: 초대 모달 추가

--- src/components/Challenge/ChallengeCard.js
+++ src/components/Challenge/ChallengeCard.js
@@ -1,7 +1,8 @@
-// ChallengeCard.js
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react'
 import "../../styles/Challenge/ChallengeCard.css";
+import InviteCodeModal from "./InviteCodeModal";
 
 export default function ChallengeCard({challenge}) {
 
@@ -11,6 +12,7 @@ export default function ChallengeCard({challenge}) {
   const endDate = new Date(challenge.end_date);
   const timeDiff = startDate.getTime() - now.getTime();
   const dayDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+  const [showInviteModal, setShowInviteModal] = useState(false);
 
   const statusLabel =
     now < startDate
@@ -19,14 +21,21 @@ export default function ChallengeCard({challenge}) {
       ? "진행중"
       : "완료";
 
+  const handleClickCard = () => {
+    if (!challenge.challenge_public) {
+      setShowInviteModal(true);
+    } else {
+      navigate(`/challenge/${challenge.challenge_id}`, {
+        state: { tab: 'info' },
+      });
+    }
+  }
+
   return (
+    <>
     <div
       className="card hover:bg-[#FAB809]"
-      onClick={() =>
-        navigate(`/challenge/${challenge.challenge_id}`, {
-          state: { tab: "info" },
-        })
-      }
+      onClick={handleClickCard}
     >
       {/* 내부 박스 */}
         <div className="card-image">
@@ -44,6 +53,15 @@ export default function ChallengeCard({challenge}) {
         <div className="w-full pl-[15px] mt-[13px] mb-[8px]">
             <p className="card-text">{challenge.tag}</p>
         </div>
+
     </div>
+    {showInviteModal && (
+        <InviteCodeModal
+          onClose={() => setShowInviteModal(false)}
+          challengeId={challenge.challenge_id}
+        />
+      )}
+  </>
   );
 }
+
