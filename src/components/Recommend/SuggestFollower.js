e197e0d7530c3b68f1f7f419924a53dd8b8696d3
Hyewonjang <hyewon5254295@gmail.com>
2025-06-12 00:27:43 +0900

    fix: recommend 관련 부분 user_id -> userId로 변경

--- src/components/Recommend/SuggestFollower.js
+++ src/components/Recommend/SuggestFollower.js
@@ -8,12 +8,12 @@ export default function SuggestFollower({searchValue, followers, participant_lis
 
     // const [followers, setFollowers] = useState([
     //     {
-    //         user_id: 2,
+    //         userId: 2,
     //         nickname: "고은츄",
     //         profileImage: "https://i.namu.wiki/i/w7GkIKr6Qac-0SCYEn7DdYBpkpZed9FaVNTBFE7aIQvm7p39bo7gs2Pb1ZWfX3dPVd0JmA3oX50T5kl-MU7wfw.webp"
     //     },
     //     {
-    //         user_id: 3,
+    //         userId: 3,
     //         nickname: "혜원츄",
     //         profileImage: "https://i.namu.wiki/i/w7GkIKr6Qac-0SCYEn7DdYBpkpZed9FaVNTBFE7aIQvm7p39bo7gs2Pb1ZWfX3dPVd0JmA3oX50T5kl-MU7wfw.webp"
     //     }
@@ -24,6 +24,8 @@ export default function SuggestFollower({searchValue, followers, participant_lis
     )
 
     const addRecommend = (targetId) => {
+        console.log("targetId", targetId);
+
         if(!(recommendList.includes(targetId))){
             setRecommendList(prev=>[...prev, targetId]);
         }
@@ -35,12 +37,12 @@ export default function SuggestFollower({searchValue, followers, participant_lis
         <div className="main h-[612px] px-[20px] flex flex-col gap-[13px] overflow-scroll">
             {(searchValue != "" ? filteredFollowers:followers).map((follower, index)=>{
                 return(
-                    <div key={follower.user_id} className="flex flex-row gap-4 py-auto align-center">
-                        <img className="flex-none w-[43px] h-[43px] rounded-full p-[1px] border-[0.5px] border-[#D9D9D9]" src={follower.profileImage} alt={`${follower.user_id}-profileImage`}/>
+                    <div key={follower.userId} className="flex flex-row gap-4 py-auto align-center">
+                        <img className="flex-none w-[43px] h-[43px] rounded-full p-[1px] border-[0.5px] border-[#D9D9D9]" src={follower.profileImage} alt={`${follower.userId}-profileImage`}/>
                         <span className="flex-1 my-auto text-[15px] text-[#6E6053] font-medium">{follower.nickname}</span>
-                        <button className={`add-recommend-btn flex-none h-fit px-5 py-[6px] my-auto text-[13px] font-medium  text-[#4A483F] ${!(participant_list.includes(follower.user_id)) && !(recommendList.includes(follower.user_id)) ? "bg-[#FAB809]" : "bg-[#F4F4F4] cursor-default"} rounded-lg`}
-                          onClick={()=>{addRecommend(follower.user_id)}}>
-                            {participant_list.includes(follower.user_id) ? "참여중" : recommendList.includes(follower.user_id) ? "권유완료" : "권유하기"}
+                        <button className={`add-recommend-btn flex-none h-fit px-5 py-[6px] my-auto text-[13px] font-medium  text-[#4A483F] ${!(participant_list.includes(follower.userId)) && !(recommendList.includes(follower.userId)) ? "bg-[#FAB809]" : "bg-[#F4F4F4] cursor-default"} rounded-lg`}
+                          onClick={()=>{addRecommend(follower.userId)}}>
+                            {participant_list.includes(follower.userId) ? "참여중" : recommendList.includes(follower.userId) ? "권유완료" : "권유하기"}
                         </button>
                     </div>
                 );
